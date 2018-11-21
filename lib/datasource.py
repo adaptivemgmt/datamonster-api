0b5a139f57a6985b93dd21c8d250a2625542d622
Josh Lindenger <jlindenger@users.noreply.github.com>
2018-11-21 11:30:50 -0500

    dm instance as an underscore attribute

--- lib/datasource.py
+++ lib/datasource.py
@@ -4,24 +4,24 @@ from .base import BaseClass
 class Datasource(BaseClass):
     """Class for a datasource"""
 
-    def __init__(self, _id, name, category, uri, dm):
+    def __init__(self, _id, name, category, uri, _dm):
         self._id = _id
         self.name = name
         self.category = category
         self.uri = uri
-        self.dm = dm
+        self.dm = _dm
 
     def get_details(self):
-        return self.dm.get_datasource_details(self._id)
+        return self._dm.get_datasource_details(self._id)
 
     @property
     def companies(self):
         """Get the data sources for this company"""
 
         if not hasattr(self, '_companies'):
-            self._companies = self.dm.get_companies(datasource=self)
+            self._companies = self._dm.get_companies(datasource=self)
 
         return self._companies
 
     def get_data(self, company, aggregation=None, start_date=None, end_date=None):
-        return self.dm.get_data(self, company, aggregation, start_date, end_date)
+        return self._dm.get_data(self, company, aggregation, start_date, end_date)
