db73efc47f6294089b4d217453b5b07569d2bed3
Josh Lindenger <jlindenger@users.noreply.github.com>
2018-11-21 11:29:51 -0500

    dm instance as an underscore attribute

--- lib/company.py
+++ lib/company.py
@@ -6,21 +6,21 @@ class Company(BaseClass):
 
     _details = None
 
-    def __init__(self, _id, ticker, name, uri, dm):
+    def __init__(self, _id, ticker, name, uri, _dm):
         self._id = _id
         self.ticker = ticker
         self.name = name
         self.uri = uri
-        self.dm = dm
+        self._dm = _dm
 
     def get_details(self):
-        return self.dm.get_company_details(self._id)
+        return self._dm.get_company_details(self._id)
 
     @property
     def datasources(self):
         """Get the data sources for this company"""
 
         if not hasattr(self, '_datasources'):
-            self._datasources = self.dm.get_datasources(company=self)
+            self._datasources = self._dm.get_datasources(company=self)
 
         return self._datasources
