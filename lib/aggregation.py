bb73002796ad8e92a7717855c464ea6810efe2f4
Josh Lindenger <jlindenger@users.noreply.github.com>
2018-11-21 11:21:59 -0500

    pr/josh/suggestions
    Since this was first commit, I can't PR directly.

--- lib/aggregation.py
+++ lib/aggregation.py
@@ -20,4 +20,4 @@ def aggregation_sanity_check(aggregation):
         raise DataMonsterError('Bad Aggregation Period: {}. Valid choices are: {}'.format(aggregation.period, aggregation_periods))
 
     if aggregation.company is not None and not isinstance(aggregation.company, Company):
-        raise DataMonsterError('Aggregation company must be Company. Found : {}'.format(aggregation.company.__class__))
+        raise DataMonsterError('Aggregation company must be an instance of Company. Found : {}'.format(aggregation.company.__class__))
