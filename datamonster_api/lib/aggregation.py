16e730578ee48a91834555bcf80c18db54f287c9
reyna <rey@adaptivemgmt.com>
2019-06-03 17:17:43 -0400

    add week agg

--- datamonster_api/lib/aggregation.py
+++ datamonster_api/lib/aggregation.py
@@ -6,6 +6,7 @@ from .errors import DataMonsterError
 
 aggregation_periods = {
     'none',
+    'week',
     'month',
     'quarter',
     'fiscalQuarter',
@@ -17,7 +18,9 @@ Aggregation = namedtuple('Aggregation', ['period', 'company'])
 
 def aggregation_sanity_check(aggregation):
     if aggregation.period is not None and aggregation.period not in aggregation_periods:
-        raise DataMonsterError('Bad Aggregation Period: {}. Valid choices are: {}'.format(aggregation.period, aggregation_periods))
+        raise DataMonsterError('Bad Aggregation Period: {}. Valid choices are: {}'.format(
+            aggregation.period, aggregation_periods))
 
     if aggregation.company is not None and not isinstance(aggregation.company, Company):
-        raise DataMonsterError('Aggregation company must be Company. Found : {}'.format(aggregation.company.__class__))
+        raise DataMonsterError('Aggregation company must be Company. Found : {}'.format(
+            aggregation.company.__class__))
