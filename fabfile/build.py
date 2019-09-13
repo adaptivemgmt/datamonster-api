044fd6f5c2719c82966c7b5f702bd8fe535b5783
eugene <eugene@adaptivemgmt.com>
2019-09-13 17:26:11 -0400

    feature/eugene/dat-2586/set-up-fab

---
+++ fabfile/build.py
@@ -0,0 +1,46 @@
+import hashlib
+import os
+import shutil
+
+from fabric.api import lcd, local, task
+
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__)) + "/.."
+
+
+@task
+def python_environment(
+    base_dir=os.path.expanduser("~/.virtualenvs"), overwrite=False, dir_name=None
+):
+    """Build Python virtualenv for the application"""
+
+    path_to_requirements = os.path.join(BASE_DIR, "requirements.txt")
+    if dir_name is None:
+
+        # This is memory inefficient. Don't put in a 5G requirements.txt file
+        req_hash = hashlib.md5(open(path_to_requirements, "rb").read()).hexdigest()
+        dir_name = os.path.join(base_dir, req_hash)
+
+    path_to_virtual_env = os.path.join(base_dir, dir_name)
+    bin_dir = os.path.join(path_to_virtual_env, "bin")
+
+    # If we are overwritting the directory, clear it out now
+    if overwrite:
+        if os.path.exists(path_to_virtual_env):
+            shutil.rmtree(path_to_virtual_env)
+
+    # If the path already exists, we're done. Return now
+    if os.path.exists(path_to_virtual_env):
+        print("path_to_virtual_env:", path_to_virtual_env)
+        return
+
+    # Make the base dir if it doesn't exist
+    if not os.path.exists(base_dir):
+        os.makedirs(base_dir)
+
+    with lcd(base_dir):
+        local("virtualenv {}".format(dir_name))
+        local("{}/pip install --upgrade pip".format(bin_dir))
+        local("{}/pip install -r {}".format(bin_dir, path_to_requirements))
+        local("virtualenv --relocatable {}".format(dir_name))
+        print("path_to_virtual_env:", path_to_virtual_env)
