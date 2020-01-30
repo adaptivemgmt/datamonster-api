c01217f72ad0e6f7b1aee7cb73ea5373d12eb591
eugene <eugene@adaptivemgmt.com>
2020-01-30 14:33:09 -0500

    bug/eugene/dat-2879/tweaks-to-dg-refresh

--- datamonster_api/lib/data_group.py
+++ datamonster_api/lib/data_group.py
@@ -77,12 +77,16 @@ class DataGroup(BaseClass):
         """
         try:
             res = self.dm.client.get(self._get_status_url())
-            if res['_id'] == self.id and res['status'] is not None:
-                self.status = res['status']
-                return self.status
         except Exception:
             raise DataMonsterError('Unknown problem fetching current status. ' +
                                    'Please contact DataMonster Customer Service.')
+
+        if res['_id'] == self.id and res['status'] is not None:
+            self.status = res['status']
+            return self.status
+
+        raise DataMonsterError('Unknown problem fetching current status. ' +
+                               'Please contact DataMonster Customer Service.')
 
     def _get_status_url(self):
         return '{}/status'.format(self.dm._get_data_group_path(self.id))
